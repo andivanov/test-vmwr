f6da6ca3f47aca01e32359f397af47985d81110b
Andrei Ivanov <z.shpack@gmail.com>
2015-06-19 00:03:58 -0700

    font awesome

---
+++ dist/scripts/839d9478.main.js
@@ -0,0 +1,1 @@
+"use strict";$(".section__vmType input").on("click",function(){$(".section__form__actions").find("a.btn.btn--disabled").removeClass("btn--disabled")}),$("#joinDomainYes").click(function(){$(this).is(":checked")&&$(this).parent().removeClass("switch-off")}),$("#joinDomainNo").click(function(){$(this).is(":checked")&&$(this).parent().addClass("switch-off")});
